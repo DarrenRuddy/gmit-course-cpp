9421aa13ed29be09f02b96201e2bb3ecbf2bac12
DarrenRuddy <74508394+DarrenRuddy@users.noreply.github.com>
2021-02-14 12:47:59 +0000

    Add files via upload

---
+++ lab-Program-Class/Labinfo.cpp
@@ -0,0 +1,12 @@
+#include "Labinfo.h"
+#include <iomanip>
+
+
+void LabInfo::printinfo(){                                                                               // Print Function //
+        cout <<setw(11)<<name <<"\n"<< labId <<"\n"<< "Date:" << setw(4)<<day <<setw(3)<< month <<setw(5)<< year <<"\n\n";   // Creating the 3 line printout to the Concole //
+    }
+void LabInfo::setdate( int daytem, int montem, int yeartem){                                                 // Function to set the date //
+    day     = daytem;
+    month   = montem;
+    year    = yeartem;
+}
