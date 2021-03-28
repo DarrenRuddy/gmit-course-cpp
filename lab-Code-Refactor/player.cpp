e0730ab80c33236a0c2abdfb933a5b727ae7b9fc
DarrenRuddy <74508394+DarrenRuddy@users.noreply.github.com>
2021-03-28 19:48:40 +0100

    Add files via upload

---
+++ lab-Code-Refactor/player.cpp
@@ -0,0 +1,65 @@
+  #include "player.h"
+
+    Player::Player(){
+        this->forename = "";
+        this->surname = "";
+        this->position="";
+        this->mobileNumber="";
+        this->dob=0;
+        this->on_team="";
+
+    }
+    Player::Player(string forename, string surname){
+        forename=forename;
+        this->surname=surname;
+    }
+
+
+    Player::Player(string forename, string surname, int mobileNumber, string position, string on_team){
+        this->forename=forename;
+        this->surname=surname;
+        this->mobileNumber=mobileNumber;
+        this->position=position;
+        this->on_team=on_team;
+    }
+
+    void Player::printPlayerInfo(){
+        cout<<"Name:"<<forename<<" "<<surname<<", DOB: "<<dob<<", position:"<<position<<", contact:  "<<mobileNumber<< on_team <<endl;
+    }
+    string Player::getForename(){
+        return forename;
+    }
+    string Player::getSurname(){
+        return surname;
+    }
+    void Player::setMobileNumber(string mobileNumber){
+        this->mobileNumber = mobileNumber;
+    }
+    string Player::getMobileNumber(){
+        return this->mobileNumber;
+    }
+    void Player::setName(string name){
+        this->forename=name;
+    }
+    void Player::setName(string forename,string surname){
+        this->forename = forename;
+        this->surname = surname;
+    }
+    void Player::setPosition(string position){
+        this->position = position;
+    }
+    string Player::getPosition(){
+        return this->position;
+    }
+    void Player::setDOB(int dob){
+       this->dob=dob;
+    }
+    int Player::getDOB(){
+        return dob;
+    }
+    void Player::seton_team(string on_team){
+        this-> on_team = on_team;
+    }
+    string Player::geton_team (){
+        return this->on_team;
+    }
---
+++ lab-Code-Refactor/player.h
@@ -0,0 +1,38 @@
+#include <iostream>
+
+using namespace std;
+
+
+
+class Player{
+private:
+    string forename;
+    string surname;
+    string position;
+    string mobileNumber;
+    string on_team;
+    int dob;
+
+public:
+    Player();
+    Player(string forename, string surname);
+    Player(string forename, string surname, int mobileNumber, string position, string on_team);
+
+    void printPlayerInfo();
+    string getForename();
+    string getSurname();
+    void setName(string name);
+    void setName(string forename,string surname);
+
+    void setPosition(string position);
+    string getPosition();
+    void setMobileNumber(string mobileNumber);
+    string getMobileNumber();
+    void seton_team(string on_team);
+    string geton_team();
+
+    void setDOB(int dob);
+    int getDOB();
+
+};
+
